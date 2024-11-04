64414b2512b01cda05c1fc8af7aaff9453f0e5c3
victor-nicola <99142809+victor-nicola@users.noreply.github.com>
2024-11-04 08:23:45 +0200

    Add files via upload

--- Nicola_Victor_Ratoni/Nicola_Victor_Ratoni/FormInformare.cs
+++ Nicola_Victor_Ratoni/Nicola_Victor_Ratoni/FormInformare.cs
@@ -16,5 +16,11 @@ namespace Nicola_Victor_Ratoni
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FormMozaic formMozaic = new FormMozaic();
+            formMozaic.ShowDialog();
+        }
     }
 }
---
+++ Nicola_Victor_Ratoni/Nicola_Victor_Ratoni/FormMozaic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nicola_Victor_Ratoni
+{
+    public partial class FormMozaic : Form
+    {
+        public FormMozaic()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            (sender as PictureBox).Width *= 2;
+            (sender as PictureBox).Height *= 2;
+            (sender as PictureBox).BringToFront();
+
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            (sender as PictureBox).Width /= 2;
+            (sender as PictureBox).Height /= 2;
+        }
+    }
+}
